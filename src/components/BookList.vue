92fb3f54858992d8a63f20020d7a1dd95cf4ca73
dcercado <87337079+dcercado@users.noreply.github.com>
2021-11-24 09:22:14 -0500

    Module real 5 Completed

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,21 +1,29 @@
 <template>
 <div>
-<h1>{{
-    title
-}}
+<h1>{{title}}
 </h1>
 <ul>
-<book-item v-for= 'book in books' :book ='book'></book-item>
 
+
+<book-item
+     v-for='book in books'
+        :key='book.id'
+        :book='book'
+      >{{book.title}}:{{book.author}}</book-item>
+</ul>
+<br><hr>
+<book-form @addBook ='appendBook'></book-form>
 </div>
 </template>
 <script>
-
+import BookForm from './BookForm';
 import BookItem from './BookItem';
 export default{
-name: "BookList", data(){
+name: "BookList", 
+data(){
 
-return {title: "All Books",
+return {
+    title: "All Books",
 books : [
     {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
     {title: 'American Gods', author: 'Neil Gaiman'},
@@ -25,7 +33,16 @@ books : [
 },
 
  components:{
-     BookItem
+     BookItem,
+  BookForm,
+  BookItem,
+ },
+ methods:{
+     appendBook(bookTitle, bookAuthor){
+         this.books.push({ title: bookTitle, author: bookAuthor })
+     }
+     
+
  }
  };
 
